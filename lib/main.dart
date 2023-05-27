c61e8122cc34c2e7eb63402e5cdc35cde845963c
ks <zavadolks@gmail.com>
2023-05-27 14:48:46 +0300

    getting metadata from photos

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-
-import 'dart:io';
-import 'dart:typed_data';
-import 'dart:ui' as ui;
-import 'package:flutter_exif_plugin/flutter_exif_plugin.dart';
-import 'package:exif/exif.dart';
 import 'package:meta_fetch/screens/home_page.dart' as home_page;
 
 void main() {
